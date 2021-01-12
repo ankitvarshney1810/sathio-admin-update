746097f61122bf17621aeed244c8dfb8299c8db4
Ankit <ankitvarshney1810@gmail.com>
2021-01-12 13:11:57 +0530

    change UI

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    $(".btnmenu").click(function(){
+    $(".side-btnmenu").click(function(){
         $(".sidebar").toggleClass('closeside');
-        $(".btnmenu").toggleClass('btnmenuclose');
+        $(".side-btnmenu").toggleClass('btnmenuclose');
         $("header").toggleClass('expand-main');
         $("#content").toggleClass('expand-main');
         $("body").toggleClass('overflow-hide');
+    });
+    $(".btnmenu").click(function(){
+        $(".nav-head").toggleClass('header-expand');
     });
 });
 
@@ -22,3 +25,31 @@ $(document).ready(function(){
         $(".refund").removeClass('border');
     });
 });
+
+// user list page start
+
+$(document).ready(function(){
+    $(".view_detail").click(function(){
+        $(".modal").css('display','flex');
+        $("body").addClass('overflow-hide');
+    });
+    $(".close").click(function(){
+        $(".modal").css('display','none');
+        $("body").removeClass('overflow-hide');
+    });
+});
+
+// user list page end
+
+// post list page start
+
+$(document).ready(function(){
+    $('#info-open-01').click(function(){
+        $('.info-open-01').css('transform','scale(1)');
+    });
+    $('#info-close-01').click(function(){
+        $('.info-open-01').css('transform','scale(0)');
+    });
+});
+
+// post list page end
